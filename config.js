3732a348785c5b63d58db538f8c31e89735733cb
dankato <apearmor@gmail.com>
2017-07-11 15:00:30 -0700

    updated url in config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+'use strict';
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/restaurants-app';
+                    //   'mongodb://localhost/restaurants-app';
+                      'mongodb://dev:devdev@ds153352.mlab.com:53352/fathomless-depths';
 exports.PORT = process.env.PORT || 8080;
